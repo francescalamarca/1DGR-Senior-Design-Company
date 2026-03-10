f59073f4f60144ee7e4ed8f19c23bcea3b6e97aa
JenniferGalindo <jgalindo@smu.edu>
2026-03-10 12:53:53 -0500

    some extra backend stuff/db

--- backend/migrations/company_profiles.sql
+++ backend/migrations/company_profiles.sql
@@ -1,21 +1,20 @@
--- scripts taken from pgAdmin create script 
 CREATE TABLE IF NOT EXISTS public.company_profiles
 (
     user_id uuid NOT NULL,
-    legal_first_name character varying(255) COLLATE pg_catalog."default",
-    legal_last_name character varying(255) COLLATE pg_catalog."default",
-    preferred_name character varying(255) COLLATE pg_catalog."default",
-    bio text COLLATE pg_catalog."default",
-    residency character varying(255) COLLATE pg_catalog."default",
-    experience text COLLATE pg_catalog."default",
-    location character varying(255) COLLATE pg_catalog."default",
-    industry_interests text COLLATE pg_catalog."default",
-    highest_education character varying(255) COLLATE pg_catalog."default",
+    company_name character varying(255),
+    industry character varying(255),
+    business_age character varying(255),
+    work_type character varying(255),
+    locations jsonb DEFAULT '[]',
+    mission_statement text,
+    core_values jsonb DEFAULT '[]',
+    benefits_summary text,
+    custom_background_color character varying(50),
+    logo_image_key character varying(500),
     created_at timestamp without time zone DEFAULT now(),
-    company_pin character varying(50) COLLATE pg_catalog."default",
     CONSTRAINT company_profiles_pkey PRIMARY KEY (user_id),
     CONSTRAINT company_profiles_user_id_fkey FOREIGN KEY (user_id)
         REFERENCES public.users (id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE CASCADE
-)
+);
